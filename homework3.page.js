f0250a77a85901666be249fc7cd3140ca67b2970
helenanovakova100 <82708815+helenanovakova100@users.noreply.github.com>
2021-07-25 13:32:13 +0200

    Update homework3.page.js

--- homework3.page.js
+++ homework3.page.js
@@ -1,7 +1,5 @@
 let helpPage = {
-    // get zajistuje, ze nemusime psat pak zavorky, coz nam rika, 
-    // ze to je jen element, ne funkce, ktera neco dela.
-    // PRIKLAD - funkce, ktera vraci element
+
     get CheckNumber() { return $('#checkSecretNumber')},
     get helpBlock(){ return $ ('#passwordHelpBlock')}, 
   }
