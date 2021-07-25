8f220e6920fee171eb90e383fbe4de62fc2b8131
helenanovakova100 <82708815+helenanovakova100@users.noreply.github.com>
2021-07-25 13:31:26 +0200

    Update homework3.js

--- homework3.js
+++ homework3.js
@@ -1,8 +1,10 @@
-//  Secret password je schovane v HTML (nedaleko inputu pro secret heslo)
-//   Je potreba ho najit a precist. Nevadi, ze neni zobrazene - data tam jsou.
-//   Cislo se generuje kazdym nactenim stranky, nestaci si jej jednou precist a 
-//     a pak ho pouzivat celou dobu. 
-//   Hlasky maji dva stavy - 1) heslo je spravne, 2) heslo je spatne.
+
+// Na přihlaš se na  webu  https://automation.cervik.repl.co/inputs.html
+// Heslo je v HTML, musi se dostat 
+// 1) přihlasit se spravnym heslem
+// 2) se špatným heslem 
+// 3) s prázdým heslem
+
 
 
 */
@@ -16,24 +18,26 @@ describe('Secret number', () => {
 
   it('confirmation appears after validating correct secret number', () => {
    
-    let Heslo = $('#secretPassword')
-    let dostanHeslo = Heslo.getValue()
-    let input = $('#secretNumberInput')
-    input.setValue(dostanHeslo)
-    helpPage.CheckNumber.click()
-    expect(helpPage.helpBlock).toHaveText('Super secret je super správně.')
+    let Heslo = $('#secretPassword') // natsevni proměné, kde má najít heslo
+    let dostanHeslo = Heslo.getValue() // proměná pro dostání hesla 
+    let input = $('#secretNumberInput') // proměná, kam se má napsat heslo 
+    
+    input.setValue(dostanHeslo) // napiš sem heslo, které si našel v HTML 
+    helpPage.CheckNumber.click() // kliknutí na potvrzení 
+    expect(helpPage.helpBlock).toHaveText('Super secret je super správně.') // ověření, že je to správné 
   });
 
   it('error appears after validating wrong secret number', () => {
-   
-    let blbeHeslo = $('#secretNumberInput')
-    blbeHeslo.setValue('140')
-    helpPage.CheckNumber.click()
-    expect(helpPage.helpBlock).toHaveText('Super secret je super špatně.')
+    let blbeHeslo = $('#secretNumberInput') // proměná kam napsat heslo 
+    
+    blbeHeslo.setValue('140') // napiš heslo 140
+    helpPage.CheckNumber.click() // kliknutí na potvrzení 
+    expect(helpPage.helpBlock).toHaveText('Super secret je super špatně.') // ověření, že je to správné 
   });
 
   it('error appears after validating empty secret number', () => {
-    helpPage.CheckNumber.click()
-    expect(helpPage.helpBlock).toHaveText('Super secret je super špatně.')
+    
+    helpPage.CheckNumber.click() // kliknutí na potvrzení 
+    expect(helpPage.helpBlock).toHaveText('Super secret je super špatně.') // ověření, že je to správné
   });
 })
