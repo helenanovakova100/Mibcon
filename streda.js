3284eb70f38fb71dd7b62cdaac60d85f6d4c2b41
helenanovakova100 <82708815+helenanovakova100@users.noreply.github.com>
2021-07-25 13:20:26 +0200

    Add files via upload

---
+++ streda.js
@@ -0,0 +1,24 @@
+
+// Napiš test, který na stránce IDOS ověří, že lze najít vlakové spojení z Prahy do Olomouce. Elementy, se kterými budeš pracovat, vyhledej co nejlépe udržovatelnými lokátory. 
+// Zdrojový text testu vykopíruj do odpovědi. 
+
+const idos = require ('../test/streda.page')
+describe('Test that we can find connection from Prague To Olomouc', () => {
+ 
+    it('Sets Values Praha and Olomouc an confirm it', () => {
+        let departure = 'Praha'
+        let arrival = 'Olomouc'
+
+        browser.url('https://idos.idnes.cz/vlakyautobusymhdvse/spojeni/');
+        $('#didomi-notice-agree-button').click() // click on accept cookies 
+        browser.maximizeWindow()
+        $('#timetablesModalLink').click()// choose transporation option 
+        $('a[href="/vlaky/spojeni/"]').scrollIntoView()
+        $('a[href="/vlaky/spojeni/"]').click() // chooses train as a connection
+        $(idos.departureValue).setValue(departure) //set value Of departure 
+        $(idos.arrivalValue).setValue(arrival) // set value of arrival  
+        $('.submit .btn').click() // click on search button 
+        expect($('p*=Praha')).toHaveTextContaining('Praha') // connection details contain Prague
+        expect($('p*=Olomouc')).toHaveTextContaining('Olomouc') // connection details contain Olomouc  
+    });
+});
---
+++ streda.page.js
@@ -0,0 +1,6 @@
+const idos = {
+    get departureValue() {return $('.inp-combined #From')},
+    get arrivalValue() {return $('.idos-autosuggest__container #To')}
+    }
+
+module.exports = idos
